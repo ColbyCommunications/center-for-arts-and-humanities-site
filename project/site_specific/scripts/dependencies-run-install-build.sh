4c858d327108eee4c6a32f3b60a5309ff05a41b4
Brandon K. Waltz <bwaltz6@gmail.com>
2025-06-23 12:23:39 -0400

    Update dependencies-run-install-build.sh

--- project/site_specific/scripts/dependencies-run-install-build.sh
+++ project/site_specific/scripts/dependencies-run-install-build.sh
@@ -3,8 +3,6 @@
 
 printf "Installing NPM dependencies for Colby dependencies"
 
-shopt -s extglob # Turns on extended globbing
-
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"  # This loads nvm
 
