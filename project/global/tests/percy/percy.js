ef8db5c55d312980a2b7ee33f46b9a0174d26fe5
Brandon K. Waltz <bwaltz6@gmail.com>
2026-01-09 12:43:57 -0500

    Update percy.js

--- project/global/tests/percy/percy.js
+++ project/global/tests/percy/percy.js
@@ -26,6 +26,7 @@ let siteFull = `https://${site}`;
 
     // Test Page
     const testPage = await browser.newPage();
+    await testPage.setUserAgent('colby-github');
     await testPage.goto(`${siteFull}/test-page`);
     await new Promise(function (resolve) {
         setTimeout(async function () {
